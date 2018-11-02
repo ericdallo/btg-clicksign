8f91609cd4c670c51313f6694f2c9151c9262126
Eric Dallo <ericdallo06@hotmail.com>
2018-11-02 20:41:28 -0300

    add ajax call do create batch

--- src/main/resources/static/js/listDocuments.js
+++ src/main/resources/static/js/listDocuments.js
@@ -1,4 +1,4 @@
-define(['doc'], function($) {
+define(['doc', 'ajax'], function($, ajax) {
 	'use strict'
 	document.addEventListener("DOMContentLoaded", function() {
 		
@@ -20,6 +20,45 @@ define(['doc'], function($) {
 			if (selectedSigner.first().checked) {
 				$('#batch-resume').addClass('signer-selected');
 			}
+		});
+		
+		$('.main-content').on('click', function(e) {
+			var isMenuEnabled = $('.signers-switch:checked').isPresent() && $('#menu').hasClass('enabled');
+			
+			if (isMenuEnabled) {
+				e.preventDefault();
+				$('#signers-switch').first().checked = false;
+			}
+		});
+		
+		$('#send-documents').on('click', function(e) {
+			e.preventDefault();
+			
+			var $sendButton = $(this);
+			
+			$sendButton.addClass('loading');
+			
+			var selectedSignerKey = $('input[name="selected-signer"]:checked').val(),
+				documentKeys = $('.documents .switch').els.map(function(document) {
+					return document.id;
+				});
+			
+			var batch = {
+				'signer': selectedSignerKey,
+				'documents': documentKeys
+			};
+			
+			ajax.post('/documents/batch', batch, {
+				'succes': function(response) {
+					console.log('success ' + response);
+				},
+				'error': function(response){
+					console.log('error ' + response);
+				},
+				'complete': function(){
+					$sendButton.removeClass('loading');
+				}
+			});
 		});
 		
 	});
